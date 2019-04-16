369cdf47b59c7529b25df79f297206cba4ec0eee
crpi <1667611552@qq.com>
2019-04-16 22:40:16 +0800

    配置可打包成jar

--- houduan/src/main/java/com/code/questionnaireSystem/QuestionnaireSystemApplication.java
+++ houduan/src/main/java/com/code/questionnaireSystem/QuestionnaireSystemApplication.java
@@ -3,12 +3,19 @@ package com.code.questionnaireSystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.code.questionnaireSystem.mapper")
-public class QuestionnaireSystemApplication {
+public class QuestionnaireSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionnaireSystemApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
--- houduan/src/main/java/com/code/questionnaireSystem/pojo/User.java
+++ houduan/src/main/java/com/code/questionnaireSystem/pojo/User.java
@@ -1,27 +1,84 @@
 package com.code.questionnaireSystem.pojo;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class User {
-    private Integer user_id;
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private String user_name;
+	private Integer user_id;
 
-    private String user_password;
+	private String user_name;
 
-    private String user_phone;
+	private String user_password;
 
-    private String user_email;
+	private String user_phone;
 
-    private Integer role_id;
+	private String user_email;
 
-    public User(Integer user_id, String user_name, String user_password, String user_phone, String user_email, Integer role_id) {
-        this.user_id = user_id;
-        this.user_name = user_name;
-        this.user_password = user_password;
-        this.user_phone = user_phone;
-        this.user_email = user_email;
-        this.role_id = role_id;
-    }
+	private Integer role_id;
+
+	public User(Integer user_id, String user_name, String user_password, String user_phone, String user_email,
+			Integer role_id) {
+		this.user_id = user_id;
+		this.user_name = user_name;
+		this.user_password = user_password;
+		this.user_phone = user_phone;
+		this.user_email = user_email;
+		this.role_id = role_id;
+	}
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	public String getUser_phone() {
+		return user_phone;
+	}
+
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public Integer getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(Integer role_id) {
+		this.role_id = role_id;
+	}
+
 }
