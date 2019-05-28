0644866bebd57829b26be596827606a962b8e69e
crpi <1667611552@qq.com>
2019-05-28 19:23:36 +0800

    改成服务器的配置

--- front/config/index.js
+++ front/config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '192.168.1.101',
+    // host: 'localhost', // can be overwritten by process.env.HOST
+    host: '192.168.1.101',
     port: 8082, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
